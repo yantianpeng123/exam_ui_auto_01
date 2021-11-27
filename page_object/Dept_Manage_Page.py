9a1a93cce954212a2d14cc0f0ca9377f76e098b1
yantianpeng <hkdyantianpeng@163.com>
2021-11-27 21:51:34 +0800

    用户管理

---
+++ page_object/Dept_Manage_Page.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# encoding: utf-8
+
+'''
+# @Time    : 2021/11/27  下午9:02
+# @Author  : yantianpeng
+# @Site    : 
+# @File    : Dept_Manage_Page.py
+# @Software: PyCharm
+'''
+$END$
